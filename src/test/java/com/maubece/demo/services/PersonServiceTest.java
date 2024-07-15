215a26bdba76ce3bd59d6c6b47151a64dcdc69e5
Mauricio Becerril <defy.mdbm@gmail.com>
2024-07-15 15:54:57 -0600

    Test template

---
+++ src/test/java/com/maubece/demo/services/PersonServiceTest.java
@@ -0,0 +1,7 @@
+package com.maubece.demo.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {}
